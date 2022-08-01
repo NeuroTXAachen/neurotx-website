ea5bcfffb85b9ed3b217739bd152d06a0826259e
Marmik Thakkar <marmik18@gmail.com>
2022-08-01 14:38:49 +0200

    added pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/navbar'
-
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
       <p>This is the homepage</p>
     </div>
-  )
+  );
 }
---
+++ pages/learn.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <p> This is learn page.</p>
+    </div>
+  );
+};
+
+export default About;
