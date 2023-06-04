de9a00932f720ad59c0559a730569a75c8a5a635
Temeos <68156424+Temeos@users.noreply.github.com>
2023-06-04 22:19:19 +0200

    Updated Team info
    Updated roles, added members and sonme links to social media

--- src/components/TeamSection/Data.js
+++ src/components/TeamSection/Data.js
@@ -3,26 +3,6 @@ export const teamDataObj = {
   waveImage: require("../../images/waves.png"),
   memberData: [
     {
-      name: "Shivam Singh Rajput",
-      title: "President",
-      image: require("../../images/team/Shivam.jpeg"),
-      description: "Master Computer Science",
-      socials: {
-        github: "#",
-        linkedIn: "#",
-      },
-    },
-    {
-      name: "Dimitar Boev",
-      title: "Vice President",
-      image: require("../../images/placeholder-image.jpeg"),
-      description: "Master Computer Science",
-      socials: {
-        github: "#",
-        linkedIn: "#",
-      },
-    },
-    {
       name: "Mohamed Alhaskir",
       title: "Founder, Software Team",
       image: require("../../images/team/Mo.jpeg"),
@@ -33,13 +13,43 @@ export const teamDataObj = {
       },
     },
     {
-      name: "Ivan Karetić",
-      title: "Hardware Team",
-      image: require("../../images/team/Ivan.jpeg"),
-      description: "Master Electrical Engineering ",
+      name: "Philipp Ljubarskij",
+      title: "President",
+      image: require("../../images/team/Philipp.jpeg"),
+      description: "Master Biomedical Engineering",
       socials: {
         github: "#",
-        linkedIn: "https://www.linkedin.com/in/ivan-karetić-52a627247",
+        linkedIn: "https://www.linkedin.com/in/philipp-ljubarskij-73a45b236",
+      },
+    },
+    {
+      name: "Akhil Verma",
+      title: "Vice President",
+      image: require("../../images/team/Akhil.jpeg"),
+      description: "Master in Robotics",
+      socials: {
+        github: "#",
+        linkedIn: "#",
+      },
+    },
+    {
+      name: "Shivam Singh Rajput",
+      title: "Head of Hardware",
+      image: require("../../images/team/Shivam.jpeg"),
+      description: "Master Computer Science",
+      socials: {
+        github: "https://github.com/shivamgif",
+        linkedIn: "https://www.linkedin.com/in/shivam-singh-rajput-a0a6271a8/",
+      },
+    },
+    {
+      name: "David Wiehls",
+      title: "Head of Software",
+      image: require("../../images/team/placeholder-image.jpeg"),
+      description: "Mechanichal Engineering",
+      socials: {
+        github: "",
+        linkedIn: "https://www.linkedin.com/in/david-wiehls",
       },
     },
     {
@@ -52,82 +62,51 @@ export const teamDataObj = {
         linkedIn: "#",
       },
     },
-
     {
-      name: "Akhil Verma",
+      name: "Fatih Kaya",
       title: "Hardware Team",
-      image: require("../../images/team/Akhil.jpeg"),
-      description: "Master in Robotics",
-      socials: {
-        github: "#",
-        linkedIn: "#",
-      },
-    },
-    {
-      name: "Josef Bolten",
-      title: "Public Relations Team",
-      image: require("../../images/team/Josef.jpeg"),
-      description: "Master in Robotics",
-      socials: {
-        github: "#",
-        linkedIn: "#",
-      },
-    },
-    {
-      name: "Marko Karetić",
-      title: "Public Relations Team",
       image: require("../../images/placeholder-image.jpeg"),
-      description: "Master Computer Science",
+      description: "Master Electrical Engineering",
       socials: {
         github: "#",
-        linkedIn: "https://www.linkedin.com/in/marko-kareti%C4%87-34849a232/",
+        linkedIn: "https://www.linkedin.com/in/fatih-kaya-a80098256",
       },
     },
     {
-      name: "Poojitha Reddy Nellipudi",
+      name: "Zeyneb Güven",
       title: "Software Team",
-      image: require("../../images/team/Poojitha.jpeg"),
-      description: "Robotic Systems Engineering",
+      image: require("../../images/team/Zeyneb.jpeg"),
+      description: "Computer Science",
       socials: {
-        github: "#",
-        linkedIn: "#",
+        github: "https://github.com/zeynebGueven",
+        linkedIn: "https://www.linkedin.com/in/zeyneb-g%C3%BCven-4186a3232/",
       },
     },
     {
-      name: "Maroof Abdul Aziz",
+      name: "Hanna Mayer",
+      title: "Harware Team",
+      image: require("../../images/placeholder-image.jpeg"),
+      description: "Computer Science",
+      socials: {
+        github: "https://github.com/zeynebGueven",
+        linkedIn: "https://www.linkedin.com/in/zeyneb-g%C3%BCven-4186a3232/",
+      },
+    },
+    {
+      name: "Liam Lee",
       title: "Hardware Team",
-      image: require("../../images/team/Maroof.jpeg"),
-      description: "Master in Robotics",
-      socials: {
-        github: "#",
-        linkedIn: "#",
-      },
-    },
-    {
-      name: "Philipp Ljubarskij",
-      title: "Software, Hardware and Management Team",
-      image: require("../../images/team/Philipp.jpeg"),
-      description: "BiomedicalEngineering",
-      socials: {
-        github: "#",
-        linkedIn: "https://www.linkedin.com/in/philipp-ljubarskij-73a45b236",
-      },
-    },
-    {
-      name: "Majd Hamza",
-      title: "Software Team",
       image: require("../../images/placeholder-image.jpeg"),
-      description: "Master Computer Science",
+      description: "Electrical Engineering Bachelor",
       socials: {
-        github: "https://github.com/majitsima",
-        linkedIn: "https://www.linkedin.com/in/majd-hamza/",
+        github: "#",
+        linkedIn: "https://www.linkedin.com/in/liam-lee-444923263",
       },
     },
     {
-      name: "Nnanna Muoneke",
-      title: "Social Media Team",
-      image: require("../../images/team/Nnanna.jpeg"),
-      description: "Technik Kommunikation",
+      name: "Jonah Schewski",
+      title: "Hardware Team",
+      image: require("../../images/placeholder-image.jpeg"),
+      description: "B.Sc. Mechanical Engineering",
       socials: {
         github: "#",
         linkedIn: "#",
@@ -143,7 +122,88 @@ export const teamDataObj = {
         linkedIn: "https://www.linkedin.com/in/usama-a-707818109",
       },
     },
+    {
+      name: "Dimitar Boev",
+      title: "Software Team",
+      image: require("../../images/placeholder-image.jpeg"),
+      description: "Master Computer Science",
+      socials: {
+        github: "#",
+        linkedIn: "#",
+      },
+    },
+    {
+      name: "Ivan Karetić",
+      title: "Hardware Team",
+      image: require("../../images/team/Ivan.jpeg"),
+      description: "Master Electrical Engineering ",
+      socials: {
+        github: "#",
+        linkedIn: "https://www.linkedin.com/in/ivan-karetić-52a627247",
+      },
+    },
+    {
+      name: "Josef Bolten",
+      title: "Hardware & Software Team",
+      image: require("../../images/team/Josef.jpeg"),
+      description: "Electronics",
+      socials: {
+        github: "#",
+        linkedIn: "LinkedIn.com/in/josef-bolten",
+      },
+    },
+    {
+      name: "Marko Karetić",
+      title: "Public Relations Team",
+      image: require("../../images/placeholder-image.jpeg"),
+      description: "Master Computer Science",
+      socials: {
+        github: "#",
+        linkedIn: "https://www.linkedin.com/in/marko-kareti%C4%87-34849a232/",
+      },
+    },
+    {
+      name: "Majd Hamza",
+      title: "Software Team",
+      image: require("../../images/placeholder-image.jpeg"),
+      description: "Master Computer Science",
+      socials: {
+        github: "https://github.com/majitsima",
+        linkedIn: "https://www.linkedin.com/in/majd-hamza/",
+      },
+    },
+    {
+      name: "Nnanna Arnold Muoneke",
+      title: "Social Media Team",
+      image: require("../../images/team/Nnanna.jpeg"),
+      description: "Technik Kommunikation M.Sc.",
+      socials: {
+        github: "#",
+        linkedIn: "https://www.linkedin.com/in/nnanna-arnold-muoneke-985947a9",
+      },
+    },
+    //{
+    //  name: "Maroof Abdul Aziz",
+    //  title: "Hardware Team",
+    //  image: require("../../images/team/Maroof.jpeg"),
+    //  description: "Master in Robotics",
+    //  socials: {
+    //    github: "#",
+    //    linkedIn: "#",
+    //  },
+    //},
+    //{
+    //  name: "Poojitha Reddy Nellipudi",
+    //  title: "Software Team",
+    //  image: require("../../images/team/Poojitha.jpeg"),
+    //  description: "Robotic Systems Engineering",
+    //  socials: {
+    //    github: "#",
+    //    linkedIn: "#",
+    //  },
+    // },
   ],
+  
   redBgPoint: require("../../images/square.png"),
   alumniData: [
     {
